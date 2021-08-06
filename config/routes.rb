42663d6482cbb8b9b8278c921bac7cf1a3a33684
Gunnar <dunkcrunk.com>
2021-08-06 10:50:41 -0600

    Had time to refactor the link to remove a plant using path helper and using a hand rolled route to have a specific path name using as: which helps clean it up. Needed to add the method to pass in for the link.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :plots, only: [:index]
   resources :gardens, only: [:show]
-  delete "/plots/:plot_id/plants/:plant_id",to: "plot_plants#destroy"
+  delete "/plots/:plot_id/plants/:plant_id",to: "plot_plants#destroy", as: 'plot_plant_delete'
 end
